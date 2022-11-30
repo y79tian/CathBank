a094f3ba2ee052cc7e75a3bd64007fb09dc89009
y79tian <y79tian@uwaterloo.ca>
2022-11-30 17:14:04 -0500

    add customized validation for api and add new version of migration file

--- api/server.go
+++ api/server.go
@@ -2,9 +2,10 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/techschool/simplebank/db/sqlc"
 )
-
 
 // Server serves each HTTP request for our banking service
 type Server struct {
@@ -17,12 +18,16 @@ func NewServer (store db.Store) *Server{
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	// router.PUT("/account/:id/:balance", server.updateAccount)
 	// router.PUT("/account/add/:id/:amount", server.addAccountBalance)
 	// router.DELETE("account/:id", server.deleteAccount)
+	router.POST("/transfers", server.createTransfer)
 	server.router = router
 	return server
 }
--- util/random.go
+++ util/random.go
@@ -43,7 +43,7 @@ func RandomMoney() int64 {
 }
 
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD"}
+	currencies := []string{EUR, CAD, USD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
