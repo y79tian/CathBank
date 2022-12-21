f5e82aba8b6cd0ba2c46728cd5f982b3082daaf9
y79tian <y79tian@uwaterloo.ca>
2022-12-21 18:27:27 -0500

    add login api

--- api/account_test.go
+++ api/account_test.go
@@ -85,7 +85,7 @@ func TestGetAccountAPI(t *testing.T) {
 			tc.buildStubs(store)
 		
 			// start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder() // record the response of the HTTP request
 		
 			url := fmt.Sprintf("/accounts/%d", tc.accountID) // func Sprintf(format string, a ...any) string
